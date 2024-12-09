2863da31156fdc494557f7d91c9a64447f9e8597
Sunnyc240048 <sunnypan0227@gmail.com>
2024-12-09 11:36:31 +0000

    main

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ app = Flask("__name__")
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("index.html"))
+    return(render_template("SA.html"))
 
 @app.route("/",methods=["GET","POST"])
 def main():
     name = request.form.get('q')
     return(render_template("main.html"))
 
+@app.route("/SA",methods=["GET","POST"])
+def SA():
+    return(render_template("main.html"))
 if __name__ == "__main__":
     app.run()
