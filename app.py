3b4660479063f43acb82767102b91a7cf6546316
Sunnyc240048 <sunnypan0227@gmail.com>
2024-12-09 12:09:01 +0000

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask("__name__")
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return(render_template("index.html"))
+    return(render_template("main.html"))
 
 @app.route("/",methods=["GET","POST"])
 def main():
@@ -16,8 +16,12 @@ def main():
 @app.route("/SA",methods=["GET","POST"])
 def SA():
     return(render_template("SA.html"))
+
+@app.route("/SA_result",methods=["GET","POST"])
 def SA_result():
     q = request.form.get('q')
     r = textblob.TextBlob(q).sentiment
+    return(render_template("SA_result.html"))
+
 if __name__ == "__main__":
     app.run()
